895202c9ba7dbda22304d06145b55b2f3b3fb905
David Ker <snappy316@gmail.com>
2023-12-12 14:30:34 -0800

    Broadcast message updates to auto-replace message in list

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
 
   after_create_commit -> { broadcast_append_to room }
   after_destroy_commit -> { broadcast_remove_to room }
+  after_update_commit -> { broadcast_replace_to room }
 end
